0f67fee5084b2829cbc46f3d392ba7010712ef96
vietlh <vietlh@localhost.localdomain>
2025-12-18 10:06:50 +0700

    Add time benchmark to main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import base64
+import time
 
 from openai import OpenAI
 
@@ -6,7 +7,7 @@ from prompt import prompt
 
 api_key = "EMPTY"
 api_base = "http://localhost:8000/v1"
-model = "Qwen/Qwen2.5-VL-3B-Instruct"
+model = "Qwen/Qwen2.5-VL-7B-Instruct"
 max_tokens = 192
 min_pixels = 3136  # 3 * 28 * 28
 max_pixels = 12845056  # 16384 * 28 * 28
@@ -20,7 +21,7 @@ with open(image_path, "rb") as f:
     b64_data = base64.b64encode(f.read()).decode("utf-8")
     b64_data = "data:image;base64," + b64_data
 
-
+start_time = time.time()
 response = client.chat.completions.create(
     model=model,
     max_tokens=max_tokens,
@@ -42,7 +43,8 @@ response = client.chat.completions.create(
         },
     ],
 )
-
+end_time = time.time()
 raw_text = response.choices[0].message.content
 print(f"image_path: {image_path}")
 print(raw_text)
+print(f"Duration: {end_time - start_time}")
